789770f032a10d35beecbcb9568ddb6ea0fa17de
Dejan Ilkovski <d.ilkovski@hotmail.com>
2022-02-26 19:27:51 +0100

    fixed errors in js scripts

--- assets/js/buttons-script.js
+++ assets/js/buttons-script.js
@@ -1,24 +1,3 @@
-// navbar
-document.querySelector(".navbar-toggler").addEventListener("click", toggleAll);
-
-function toggleAll() {
-  var logoVisibility = document.querySelector(".navbar-brand").style.visibility;
-  if (logoVisibility == "visible" || logoVisibility == "") {
-    document.querySelector(".navbar-brand").style.visibility = "hidden";
-  } else {
-    document.querySelector(".navbar-brand").style.visibility = "visible";
-  }
-
-  var displayAll = document.querySelector(".none").style.display;
-  if (displayAll == "inline-block" || displayAll == "") {
-    document.querySelector(".none").style.display = "none";
-  } else {
-    document.querySelector(".none").style.display = "inline-block";
-  }
-
-  document.querySelector(".navbar").classList.toggle("nav-shown");
-  document.querySelector(".navbar").classList.toggle("bg-dark");
-}
 
 // filters cards
 document.querySelector("#filter-coding").addEventListener("click", showCoding);
---
+++ assets/js/navbar-script.js
@@ -0,0 +1,24 @@
+// navbar
+document.querySelector(".navbar-toggler").addEventListener("click", toggleAll);
+
+function toggleAll() {
+  var logoVisibility = document.querySelector(".navbar-brand").style.visibility;
+  if (logoVisibility == "visible" || logoVisibility == "") {
+    document.querySelector(".navbar-brand").style.visibility = "hidden";
+  } else {
+    document.querySelector(".navbar-brand").style.visibility = "visible";
+  }
+
+  var displayAll = document.querySelector(".none").style.display;
+  if (displayAll == "inline-block" || displayAll == "") {
+    document.querySelector(".none").style.display = "none";
+  } else {
+    document.querySelector(".none").style.display = "inline-block";
+  }
+
+  document.querySelector(".navbar").classList.toggle("nav-shown");
+  document.querySelector(".navbar").classList.toggle("bg-dark");
+}
+
+
+
